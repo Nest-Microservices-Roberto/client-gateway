07ce57cb7f6fd89fb29037709f35c302610ac3fe
R-Berrocal <robertoberro20@gmail.com>
2024-06-02 11:22:41 -0500

    manejo de excepciones

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { envs } from './config';
+import { RpcCustomExcpetionFilter } from './common';
+
+async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.useGlobalFilters(new RpcCustomExcpetionFilter());
+  await app.listen(envs.port);
+  logger.log(`Gateway running on port: ${envs.port}`);
+}
+bootstrap();
